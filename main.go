5766d2a4e7edec77f592f1ab98f6900a9d81b5db
Germain Leignel <germain.leignel@gmail.com>
2026-02-01 12:49:49 +0100

    chore: trigger pipeline

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Tekton!")
+		fmt.Fprintf(w, "Hello from Tekton! (build %s)", "2026-02-01T11:48:00Z")
 	})
 
 	log.Println("Server starting on port 8080...")
